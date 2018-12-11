396b0d53cd73703566e3ba0e23e225d6c5b2b706
Ramya Ashok Rao <rrao@expedia.com>
2018-12-10 20:48:59 -0600

    more design changes

--- Rock-paper-scissors.js
+++ Rock-paper-scissors.js
@@ -7,9 +7,9 @@ $(document).ready(function() {
     function randomGen(){
         var num = Math.round(Math.random()* (3 - 1) + 1);
 
-        if(num ==1 ){
+        if(num == 1 ){
             return "Rock";
-        } else if(num ==2){
+        } else if(num == 2){
             return "Paper";
         } else {
             return "Scissors";
@@ -18,7 +18,7 @@ $(document).ready(function() {
 
     function revealResult(p,c){
         if( p == c){
-            $("#result").text("Its a tie try again");
+            $("#result p").text("Its a tie try again");
         } else {
 
             var choices =[p,c];
@@ -27,22 +27,48 @@ $(document).ready(function() {
             var scissors= $.inArray("Scissors", choices);
 
             if(rock >= 0 && paper >= 0){
-                rock < paper ? $("#result").text("I won, Human lost"): $("#result").text("You won, not bad");
+                rock < paper ? $("#result p").text("I won, Human lost"): $("#result p").text("You won, not bad");
             }
             else if (rock >= 0 && scissors >= 0) {
-                rock < scissors ? $("#result").text("You won, not bad"): $("#result").text("I won, Human lost");
+                rock < scissors ? $("#result p").text("You won, not bad"): $("#result p").text("I won, Human lost");
             } else if (paper >=0 && scissors >=0) {
-                scissors < paper ? $("#result").text("You won, not bad"): $("#result").text("I won, Human lost");
+                scissors < paper ? $("#result p").text("You won, not bad"): $("#result p").text("I won, Human lost");
             }
+        }
+    }
+
+    function styleResultBoxes($this, playersChoice, computersChoice) {
+        //adding border when clicked
+        $("#options p").removeClass("highlighted");
+        $this.addClass("highlighted");
+
+        //adding text in result boxes
+        $("#players-choice p").text(playersChoice);
+        $("#computers-choice p").text(computersChoice);
+
+        //adding background img
+        $("#players-choice, #computers-choice").removeClass("rock-background paper-background scissors-background");
+        if(playersChoice == "Rock"){
+            $("#players-choice").addClass("rock-background");
+        } else if(playersChoice == "Paper") {
+            $("#players-choice").addClass("paper-background");
+        } else {
+            $("#players-choice").addClass("scissors-background");
+        }
+
+        if(computersChoice == "Rock"){
+            $("#computers-choice").addClass("rock-background");
+        } else if(computersChoice == "Paper") {
+            $("#computers-choice").addClass("paper-background");
+        } else {
+            $("#computers-choice").addClass("scissors-background");
         }
     }
 
     $( "p" ).click(function(e) {
         var playersChoice = $(this).html();
         var computersChoice = randomGen();
-        $("#players-choice").text(playersChoice);
-        $("#computers-choice").text(computersChoice);
-
+        styleResultBoxes($(this),playersChoice,computersChoice);
         revealResult(playersChoice,computersChoice);
     });
 });
